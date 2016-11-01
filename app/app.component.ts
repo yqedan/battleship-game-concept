ab87fa1fe624d2da0a7906092a3622e28710d21f
Yusuf Qedan <Yusuf Qedan>
2016-10-31 22:45:38 -0700

    remove not needed code, fix indenting, tweak styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,25 +10,27 @@ import { Game } from './game.model';
       <h1>Battleship</h1>
     </div>
     <div class="well">
-    <table align="center">
-      <tr>
-        <td></td>
-        <td align="center" *ngFor="let foo of dummyArray; let index = index">
-          {{index+1}}
-        </td>
-      </tr>
-      <tr *ngFor="let letter of letterArray; let row = index">
-        <td align="center">{{letter}}</td>
-        <td (click)=fire(myGame.board[row][col],row,col) align="center" *ngFor=" let currentSquare of myGame.board[row]; let col = index"
-        [class.hit]="myGame.board[row][col].hit"
-        [class.ship]="myGame.board[row][col].ship"
-        [class.miss]="myGame.board[row][col].miss"
-        >
-        </td>
-      </tr>
-    </table>
+      <table align="center">
+        <tr>
+          <td></td>
+          <td align="center" *ngFor="let foo of dummyArray; let index = index">
+            {{index+1}}
+          </td>
+        </tr>
+        <tr *ngFor="let letter of letterArray; let row = index">
+          <td align="center">{{letter}}</td>
+          <td (click)=fire(myGame.board[row][col],row,col) align="center"
+          *ngFor=" let currentSquare of myGame.board[row]; let col = index"
+          [class.hit]="myGame.board[row][col].hit"
+          [class.ship]="myGame.board[row][col].ship"
+          [class.miss]="myGame.board[row][col].miss"
+          >
+          </td>
+        </tr>
+      </table>
+      <br>
+      <button class="btn"(click)="generateFakeShips()">Generate non-random ships!</button>
     </div>
-     <button class="btn"(click)="generateFakeShips()">Generate non-random ships!</button>
   </div>
   `
 })
@@ -38,10 +40,16 @@ export class AppComponent {
   public dummyArray = new Array(10);
   public letterArray = ["A","B","C","D","E","F","G","H","I","J"];
   generateFakeShips(){
+    this.myGame.board[9][2].ship = true;
+    this.myGame.board[9][3].ship = true;
+
+    this.myGame.board[7][6].ship = true;
+    this.myGame.board[8][6].ship = true;
+    this.myGame.board[9][6].ship = true;
+
     this.myGame.board[3][3].ship = true;
     this.myGame.board[4][3].ship = true;
     this.myGame.board[5][3].ship = true;
-    this.myGame.board[3][3].ship = true;
 
     this.myGame.board[0][1].ship = true;
     this.myGame.board[0][2].ship = true;
@@ -53,13 +61,6 @@ export class AppComponent {
     this.myGame.board[3][7].ship = true;
     this.myGame.board[4][7].ship = true;
     this.myGame.board[5][7].ship = true;
-
-    this.myGame.board[9][2].ship = true;
-    this.myGame.board[9][3].ship = true;
-
-    this.myGame.board[7][6].ship = true;
-    this.myGame.board[8][6].ship = true;
-    this.myGame.board[9][6].ship = true;
   }
   fire(selectedSquare:Square,row: number,col: number){
     if (selectedSquare.ship === true) {
--- app/square.model.ts
+++ app/square.model.ts
@@ -2,7 +2,4 @@ export class Square{
   public ship:boolean = false;
   public hit:boolean = false;
   public miss:boolean = false;
-  constructor(){
-
-  }
 }
